e14dee968ed841c5ba420db9166e6d0cf756a368
Maze <35577210+MazeLu@users.noreply.github.com>
2023-12-03 23:41:14 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,316 @@
+import WindowManager from './WindowManager.js'
+
+
+const t = THREE;
+let camera, scene, renderer, world;
+let near, far;
+let pixR = window.devicePixelRatio ? window.devicePixelRatio : 1;
+let universes = [];
+let sceneOffsetTarget = {x: 0, y: 0};
+let sceneOffset = {x: 0, y: 0};
+
+let today = new Date();
+today.setHours(0);
+today.setMinutes(0);
+today.setSeconds(0);
+today.setMilliseconds(0);
+today = today.getTime();
+
+let internalTime = getTime();
+let windowManager;
+let initialized = false;
+
+function getTime ()
+{
+	return (new Date().getTime() - today) / 1000.0;
+}
+
+
+if (new URLSearchParams(window.location.search).get("clear"))
+{
+	localStorage.clear();
+}
+else
+{	
+	document.addEventListener("visibilitychange", () => 
+	{
+		if (document.visibilityState != 'hidden' && !initialized)
+		{
+			init();
+		}
+	});
+
+	window.onload = () => {
+		if (document.visibilityState != 'hidden')
+		{
+			init();
+		}
+	};
+	
+
+	function init ()
+	{
+		initialized = true;
+
+		// add a short timeout because window.offsetX reports wrong values before a short period 
+		setTimeout(() => {
+			setupScene();
+			// createParticleSystem();
+			setupWindowManager();
+			resize();
+			updateWindowShape(false);
+			render();
+			window.addEventListener('resize', resize);
+		}, 500)	
+	}
+	
+
+	function setupScene ()
+	{
+		camera = new t.OrthographicCamera(0, 0, window.innerWidth, window.innerHeight, -10000, 10000);
+		
+		camera.position.z = 2.5;
+		near = camera.position.z - .5;
+		far = camera.position.z + 0.5;
+
+		scene = new t.Scene();
+		scene.background = new t.Color(0.0);
+		scene.add( camera );
+
+		var starGeometry = new THREE.Geometry();
+		for (let i = 0; i < 5000; i++) {
+			var star = new THREE.Vector3();
+			star.x = Math.random() * 5000 - 2000;
+			star.y = Math.random() * 5000 - 2000;
+			star.z = Math.random() * 5000 - 2000;
+			starGeometry.vertices.push(star);
+			var color = new THREE.Color();
+			if (Math.random() < 0.5) {
+				color.setHSL(0.16, 0.5, Math.random() * 0.5 + 0.25);
+			} else {
+				color.setHSL(0.0, 0.0, Math.random() * 0.5 + 0.5);
+			}
+			starGeometry.colors.push(color);
+		}
+		
+		var starMaterial = new THREE.PointsMaterial({
+			size: 2,
+			vertexColors: THREE.VertexColors
+		});
+		
+		var starField = new THREE.Points(starGeometry, starMaterial);
+		scene.add(starField);
+	
+		renderer = new t.WebGLRenderer({antialias: true, depthBuffer: true});
+		renderer.setPixelRatio(pixR);
+		
+		world = new t.Object3D();
+		scene.add(world);
+	
+		renderer.domElement.setAttribute("id", "scene");
+		document.body.appendChild( renderer.domElement );
+
+		// Lights
+		var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+		scene.add( light );
+
+		var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+		directionalLight.position.set( 0, 128, 128 );
+		scene.add( directionalLight );
+		
+	}
+
+	
+	function setupWindowManager ()
+	{
+		windowManager = new WindowManager();
+		windowManager.setWinShapeChangeCallback(updateWindowShape);
+		windowManager.setWinChangeCallback(windowsUpdated);
+
+		// here you can add your custom metadata to each windows instance
+		let metaData = {foo: "bar"};
+
+		// this will init the windowmanager and add this window to the centralised pool of windows
+		windowManager.init(metaData);
+
+		// call update windows initially (it will later be called by the win change callback)
+		windowsUpdated();
+	}
+
+	
+	function windowsUpdated ()
+	{
+		updateNumberOfUniverses();
+	}
+
+	
+	function updateNumberOfUniverses ()
+	{
+		let wins = windowManager.getWindows();
+
+		universes.forEach((c) => {
+			world.remove(c);
+		})
+
+		universes = [];
+
+		for (let i = 0; i < wins.length; i++)
+		{
+			let win = wins[i];
+
+			let c;
+			if (i == 0) {
+				c = new t.Color('hsl(230, 80%, 75%)');
+			} else if (i == 1) {
+				c = new t.Color('hsl(350, 60%, 65%)');
+			} else {
+				let idBasedHueValue = (win.id % 10) / 10;
+				let hue;
+				if(idBasedHueValue < 0.5) {
+					hue = 240 - (idBasedHueValue * 2 * 60);
+				} else {
+					hue = 360 - ((idBasedHueValue - 0.5) * 2 * 60);
+				}
+				c = new t.Color(`hsl(${hue}, 50%, 70%)`);
+			}
+
+			let s = 100 + i * 50;
+			let radius = s / 2;
+
+			let sphere = createComplexSphere(radius, c);
+			sphere.position.x = win.shape.x + (win.shape.w * .5);
+			sphere.position.y = win.shape.y + (win.shape.h * .5);
+	
+			world.add(sphere);
+			universes.push(sphere);
+
+		}
+	}
+
+	
+	function createComplexSphere(radius, color) {
+		let outerSize = radius;
+		let outerColor = color;
+	
+		let complexSphere = new THREE.Group();
+		let sphereWireframeOuter = new THREE.Mesh(
+			new THREE.IcosahedronGeometry(outerSize, 3),
+			new THREE.MeshLambertMaterial({
+				color: outerColor,
+				wireframe: true,
+				transparent: true,
+				shininess: 0
+			})
+		);
+		complexSphere.add(sphereWireframeOuter);
+		let sphereGlassOuter = new THREE.Mesh(
+			new THREE.SphereGeometry(outerSize, 32, 32),
+			new THREE.MeshPhongMaterial({
+				color: outerColor,
+				transparent: true,
+				shininess: 25,
+				opacity: 0.3
+			})
+		);
+		complexSphere.add(sphereGlassOuter);
+
+		let particlesOuter = createParticles(outerSize, outerColor);
+		complexSphere.add(particlesOuter);
+
+		return complexSphere;
+	}
+	
+	function createParticles(size, color) {
+		let geometry = new THREE.Geometry();
+		for (let i = 0; i < 35000; i++) {
+			let x = -1 + Math.random() * 2;
+			let y = -1 + Math.random() * 2;
+			let z = -1 + Math.random() * 2;
+			let d = 1 / Math.sqrt(x * x + y * y + z * z);
+			x *= d * size;
+			y *= d * size;
+			z *= d * size;
+			geometry.vertices.push(new THREE.Vector3(x, y, z));
+		}
+		let material = new THREE.PointsMaterial({
+			size: 0.1,
+			color: color,
+			transparent: true
+		});
+		return new THREE.Points(geometry, material);
+	}
+	
+
+	function updateWindowShape (easing = true)
+	{
+		sceneOffsetTarget = {x: -window.screenX, y: -window.screenY};
+		if (!easing) sceneOffset = sceneOffsetTarget;
+	}
+
+
+	function render ()
+	{
+		let t = getTime();
+
+		windowManager.update();
+
+		let falloff = .05;
+		sceneOffset.x = sceneOffset.x + ((sceneOffsetTarget.x - sceneOffset.x) * falloff);
+		sceneOffset.y = sceneOffset.y + ((sceneOffsetTarget.y - sceneOffset.y) * falloff);
+
+		world.position.x = sceneOffset.x;
+		world.position.y = sceneOffset.y;
+
+		let wins = windowManager.getWindows();
+
+		for (let i = 0; i < universes.length; i++)
+		{
+			let complexSphere = universes[i]; 
+			let win = wins[i];
+			let _t = t; 
+
+			let posTarget = {x: win.shape.x + (win.shape.w * .5), y: win.shape.y + (win.shape.h * .5)}
+			
+			complexSphere.position.x = complexSphere.position.x + (posTarget.x - complexSphere.position.x) * falloff;
+      complexSphere.position.y = complexSphere.position.y + (posTarget.y - complexSphere.position.y) * falloff;
+			complexSphere.rotation.x = _t * .5; 
+			complexSphere.rotation.y = _t * .3; 
+      
+			updateComplexSphere(complexSphere, t);
+		};
+    
+		renderer.render(scene, camera);
+		requestAnimationFrame(render);
+	}
+
+	
+	function updateComplexSphere(complexSphere, elapsedTime) {
+		let sphereWireframeOuter = complexSphere.children[0];
+		let sphereGlassOuter = complexSphere.children[1];
+		let particlesOuter = complexSphere.children[2];
+		sphereWireframeOuter.rotation.x += 0.001;
+		sphereWireframeOuter.rotation.z += 0.001;
+		sphereGlassOuter.rotation.y += 0.01;
+		sphereGlassOuter.rotation.z += 0.01;
+	
+		particlesOuter.rotation.y += 0.0005;
+		var outerShift = Math.abs(Math.cos(((elapsedTime + 5) / 10)));
+	
+		sphereWireframeOuter.material.color.setHSL(0.55, 1, outerShift);
+		sphereGlassOuter.material.color.setHSL(0.55, 1, outerShift);
+		particlesOuter.material.color.setHSL(0.55, 1, outerShift);
+
+		sphereWireframeOuter.material.opacity = Math.abs(Math.cos(elapsedTime / 0.9) * 0.5);
+	
+	}
+	
+		function resize ()
+	{
+		let width = window.innerWidth;
+		let height = window.innerHeight
+		
+		camera = new t.OrthographicCamera(0, width, 0, height, -10000, 10000);
+		camera.updateProjectionMatrix();
+		renderer.setSize( width, height );
+	}
+}
